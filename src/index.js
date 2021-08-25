865cb84ea377ff7d905b3ded5d77a2e5253a6866
hiroyuki12 <hiroyuki12@gmail.com>
2021-08-25 22:12:45 +0900

    Update index.js.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Qiita from './page/qiita/Qiita';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Qiita />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
