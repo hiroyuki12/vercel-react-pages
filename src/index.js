e65fadad6f8b67f742bd9ba46cf354b85b4d00e5
hiroyuki12 <hiroyuki12@gmail.com>
2021-08-28 02:18:10 +0900

    Add Qiita.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Qiita from './page/qiita/Qiita';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Qiita />
   </React.StrictMode>,
   document.getElementById('root')
 );
